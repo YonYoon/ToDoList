bf820fc94ef1df318e15b93acd7bfb4d8b11c5fd
YonYoon <zhzhzh1806@gmail.com>
2023-10-08 16:22:24 +0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Zhansen Zhalel on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
